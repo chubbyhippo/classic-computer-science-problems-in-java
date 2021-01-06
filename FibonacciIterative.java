a766b48b2489e9ca059d9e2e6e2cfef3df7efc98
ch <mkmsacc@outlook.com>
2021-01-06 16:16:33 +0700

    Add iterative approach fib

--- FibonacciIterative.java
+++ FibonacciIterative.java
@@ -1,9 +1,13 @@
-public class FibonacciRecursive {
+public class FibonacciIterative {
+
     private static int fib(int n) {
-        if (n < 2) {
-            return n;
+        int last = 0, next = 1; // fib(0), fib(1)
+        for (int i = 0; i < n; i++) {
+            int oldLast = last;
+            last = next;
+            next = oldLast + next;
         }
-        return fib(n - 1) + fib(n - 2);
+        return last;
     }
 
     public static void main(String[] args) {
@@ -12,4 +16,5 @@ public class FibonacciRecursive {
         long elapsedTime = System.nanoTime() - start;
         System.out.println(elapsedTime);
     }
+
 }
