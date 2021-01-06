d9c5f66e08e820c3c7160bd78e7e6520e7aafc65
ch <mkmsacc@outlook.com>
2021-01-06 21:14:13 +0700

    Add Fibonacci stream

---
+++ FibonacciStream.java
@@ -0,0 +1,21 @@
+import java.util.stream.IntStream;
+
+public class FibonacciStream {
+
+    private int last = 0, next = 1; // fib(0), fib(1)
+
+    public IntStream stream() {
+        return IntStream.generate(() -> {
+            int oldLast = last;
+            last = next;
+            next = oldLast + next;
+            return oldLast;
+        });
+    }
+
+    public static void main(String[] args) {
+        FibonacciStream fib = new FibonacciStream();
+        fib.stream().limit(41).forEachOrdered(System.out::println);
+    }
+
+}
