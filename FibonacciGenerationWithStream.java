1a04d1c4b781d1a843280d74c58698e9ea7c68fd
ch <mkmsacc@outlook.com>
2021-01-07 20:48:48 +0700

    Change name

--- FibonacciGenerationWithStream.java
+++ FibonacciGenerationWithStream.java
@@ -1,6 +1,6 @@
 import java.util.stream.IntStream;
 
-public class FibonacciStream {
+public class FibonacciGenerationWithStream {
 
     private int last = 0, next = 1; // fib(0), fib(1)
 
@@ -14,7 +14,7 @@ public class FibonacciStream {
     }
 
     public static void main(String[] args) {
-        FibonacciStream fib = new FibonacciStream();
+        FibonacciGenerationWithStream fib = new FibonacciGenerationWithStream();
         fib.stream().limit(41).forEachOrdered(System.out::println);
     }
 
