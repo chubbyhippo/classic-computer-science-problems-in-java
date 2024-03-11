3f7d0258713dd95f870692f86aa007e998516406
ch <mkmsacc@outlook.com>
2024-03-11 15:20:40 +0700

    Refactor FibonacciRecursive and remove main method
    Converted FibonacciRecursive class to utility class and changed visibility of the fib method from private to public. Simultaneously, the main method used for testing the function has been removed. This improves the overall structure of the code and prevents object instantiation of the class.

--- src/main/java/com/example/FibonacciRecursive.java
+++ src/main/java/com/example/FibonacciRecursive.java
@@ -1,17 +1,14 @@
 package com.example;
 
 public class FibonacciRecursive {
-    private static int fib(int n) {
+    private FibonacciRecursive() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static int fib(int n) {
         if (n < 2) {
             return n;
         }
         return fib(n - 1) + fib(n - 2);
-    }
-
-    public static void main(String[] args) {
-        var start = System.nanoTime();
-        System.out.println(fib(40));
-        var elapsedTime = System.nanoTime() - start;
-        System.out.println(elapsedTime);
     }
 }
