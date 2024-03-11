c5c1b3af3df673ea3f1f60ff5304690a8f8a2c18
ch <mkmsacc@outlook.com>
2024-03-11 15:19:26 +0700

    Add unit tests for FibonacciRecursive function.
    A new file, FibonacciRecursiveTest.java, has been added in the 'com.example' package to include unit tests for the recursive Fibonacci function. These tests are developed to make sure the function runs without throwing any exceptions.

---
+++ src/test/java/com/example/FibonacciRecursiveTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.example.FibonacciRecursive.fib;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class FibonacciRecursiveTest {
+
+    @Test
+    @DisplayName("should return fib recursive")
+    void shouldReturnFibRecursive() {
+
+        var start = System.nanoTime();
+        System.out.println(fib(40));
+        var elapsedTime = System.nanoTime() - start;
+        System.out.println(elapsedTime);
+
+        assertThatCode(() -> fib(1)).doesNotThrowAnyException();
+    }
+
+}
