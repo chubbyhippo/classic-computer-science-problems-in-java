dc28d7cfb710542a519f9df4837eef309bcc46ea
ch <mkmsacc@outlook.com>
2024-03-11 20:42:16 +0700

    Modify FibonacciIterivative.java for testability
    Transformed FibonacciIterative.java by making its constructor private and removing the main method. This refactoring is done to prevent the instantiation of FibonacciIterative class and enable its testing with a dedicated test class. Fib method is now public supporting better unit testing.

--- src/main/java/com/example/FibonacciIterative.java
+++ src/main/java/com/example/FibonacciIterative.java
@@ -2,21 +2,19 @@ package com.example;
 
 public class FibonacciIterative {
 
-    private static int fib(int n) {
-        int last = 0, next = 1; // fib(0), fib(1)
+    private FibonacciIterative() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static int fib(int n) {
+        int last = 0;
+        int next = 1;
         for (var i = 0; i < n; i++) {
             var oldLast = last;
             last = next;
             next = oldLast + next;
         }
         return last;
-    }
-
-    public static void main(String[] args) {
-        var start = System.nanoTime();
-        System.out.println(fib(40));
-        var elapsedTime = System.nanoTime() - start;
-        System.out.println(elapsedTime);
     }
 
 }
