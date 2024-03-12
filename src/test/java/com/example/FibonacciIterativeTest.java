6327b666f6ef31531ae57da31a1b5d1d29b2c50a
ch <mkmsacc@outlook.com>
2024-03-12 20:23:54 +0700

    Add test for illegalStateException in FibonacciIterative
    Added a new test method in FibonacciIterativeTest to verify the IllegalStateException. This test confirms whether the class throws an exception when an unexpected circumstance arises, which is an important aspect of robust software design.

--- src/test/java/com/example/FibonacciIterativeTest.java
+++ src/test/java/com/example/FibonacciIterativeTest.java
@@ -5,12 +5,28 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
+
 import static com.example.FibonacciIterative.fib;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FibonacciIterativeTest {
 
     private static final Logger log = LoggerFactory.getLogger(FibonacciIterativeTest.class);
+
+    @Test
+    @DisplayName("should throw illegal state exception when initialized")
+    void shouldThrowIllegalStateExceptionWhenInitialized() {
+        var constructor = FibonacciIterative.class.getDeclaredConstructors()[0];
+        constructor.setAccessible(true);
+        try {
+            constructor.newInstance();
+        } catch (IllegalStateException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException exception) {
+            assertThat(exception.getCause().getClass()).isEqualTo(IllegalStateException.class);
+            assertThat(exception.getCause().getMessage()).isEqualTo("Utility class");
+        }
+    }
 
     @Test
     @DisplayName("should return fib iterative")
