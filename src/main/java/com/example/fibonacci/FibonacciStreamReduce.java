01771cd2938d8da3015a9d3447d8ff30d1f44c02
ch <mkmsacc@outlook.com>
2024-03-12 22:01:34 +0700

    Rename FibonacciStream to FibonacciStreamReduce and refactor
    Renamed the class FibonacciStream to FibonacciStreamReduce to reflect its functionality more accurately. Removed logger dependency, changed public main method to private constructor and transformed the void method to return the calculated Fibonacci of inputted number, improving overall code organization and purpose.

--- src/main/java/com/example/fibonacci/FibonacciStreamReduce.java
+++ src/main/java/com/example/fibonacci/FibonacciStreamReduce.java
@@ -1,15 +1,14 @@
 package com.example.fibonacci;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.stream.IntStream;
 
-public class FibonacciStream {
-    private static final Logger log = LoggerFactory.getLogger(FibonacciStream.class);
+public class FibonacciStreamReduce {
 
-    public static void main(String[] args) {
-        int n = 40;  // Fibonacci number to find
+    private FibonacciStreamReduce() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static int fib(int n) {
 
         int[] fibonacci = IntStream.rangeClosed(1, n)
                 .boxed()
@@ -17,8 +16,7 @@ public class FibonacciStream {
                         (a, c) -> new int[]{a[1], a[0] + a[1]},
                         (a, b) -> a
                 );
-
-        log.info("{}", fibonacci[0]);
+        return fibonacci[0];
     }
 
 }
