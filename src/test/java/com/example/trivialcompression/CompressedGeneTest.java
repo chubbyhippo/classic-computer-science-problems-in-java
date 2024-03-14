c0474943375251dc3e8f7c96c495d4cd09a42507
ch <mkmsacc@outlook.com>
2024-03-14 20:59:30 +0700

    Add CompressedGeneTest class for compression tests
    A new class, CompressedGeneTest.java, has been introduced under the 'trivialcompression' package. This includes a unit test to ensure that compression and decompression methods work correctly. The test verifies that, after decompression, the original input string is obtained, ensuring the accuracy of the compression-decompression process.

---
+++ src/test/java/com/example/trivialcompression/CompressedGeneTest.java
@@ -0,0 +1,18 @@
+package com.example.trivialcompression;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CompressedGeneTest {
+
+    @Test
+    @DisplayName("should compress and decompress")
+    void shouldCompressAndDecompress() {
+        final var original = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA";
+        var compressed = new CompressedGene(original);
+        final var decompressed = compressed.decompress();
+        assertThat(original).isEqualTo(decompressed);
+    }
+}
