5fcfa3cba6366d663fa01d46743561327e545f94
ch <mkmsacc@outlook.com>
2024-03-14 21:24:20 +0700

    Add Leibniz formula comment to PiCalculator
    In this commit, a comment detailing the Leibniz formula for calculating Pi has been added to the PiCalculator class. This additional context will help developers understand the mathematical foundation of the Pi calculation used in this utility class.

--- src/main/java/com/example/pi/PiCalculator.java
+++ src/main/java/com/example/pi/PiCalculator.java
@@ -1,5 +1,6 @@
 package com.example.pi;
 
+// Leibniz formula, π = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11...
 public class PiCalculator {
     private PiCalculator() {
         throw new IllegalStateException("Utility class");
