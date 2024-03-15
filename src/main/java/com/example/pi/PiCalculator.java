bbe25f943f70e585a606a8968ebada5099eab6d9
ch <mkmsacc@outlook.com>
2024-03-15 14:44:28 +0700

    Your commit message is incorrect based on the provided diff/code changes. Based on the diff you have given, the appropriate commit message should be:
    Add calculatePi method to PiCalculator
    
    Added a new static method 'calculatePi' to the PiCalculator utility class. This method calculates and returns the value of pi based on the number of terms provided, using a numerical series. This should enable dynamic and precise calculations of the Pi constant for various applications.

--- src/main/java/com/example/pi/PiCalculator.java
+++ src/main/java/com/example/pi/PiCalculator.java
@@ -5,4 +5,17 @@ public class PiCalculator {
     private PiCalculator() {
         throw new IllegalStateException("Utility class");
     }
+
+    public static double calculatePi(int nTerms) {
+        final double numerator = 4.0;
+        double denominator = 1.0;
+        double operation = 1.0;
+        double pi = 0.0;
+        for (int i = 0; i < nTerms; i++) {
+            pi += operation * (numerator / denominator);
+            denominator += 2.0;
+            operation *= -1.0;
+        }
+        return pi;
+    }
 }
