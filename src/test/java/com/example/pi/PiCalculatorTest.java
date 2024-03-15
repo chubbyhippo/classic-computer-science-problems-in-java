512e6f57120ae41974464a89bef62494e8eef88f
ch <mkmsacc@outlook.com>
2024-03-15 14:44:05 +0700

    Add logger and new unit test to PiCalculatorTest
    Added LoggerFactory logger to PiCalculatorTest to log calculated pi value. Also, added a new test method 'shouldReturnPiCalculationResultFromNTerms' that calculates and verifies the value of pi using 'calculatePi' function with maximum integer value. These additions aid in better tracking of pi calculations and ensuring the correctness of the calculation function.

--- src/test/java/com/example/pi/PiCalculatorTest.java
+++ src/test/java/com/example/pi/PiCalculatorTest.java
@@ -2,12 +2,16 @@ package com.example.pi;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PiCalculatorTest {
+    private static final Logger log = LoggerFactory.getLogger(PiCalculatorTest.class);
+
     @Test
     @DisplayName("should throw illegal state exception when initialized")
     void shouldThrowIllegalStateExceptionWhenInitialized() {
@@ -20,6 +24,17 @@ class PiCalculatorTest {
             assertThat(exception.getCause().getClass()).isEqualTo(IllegalStateException.class);
             assertThat(exception.getCause().getMessage()).isEqualTo("Utility class");
         }
+    }
+
+    @Test
+    @DisplayName("should return pi calculation result from n terms")
+    void shouldReturnPiCalculationResultFromNTerms() {
+
+        var pi = PiCalculator.calculatePi(Integer.MAX_VALUE);
+        log.info("Leibniz PI : {}", pi);
+        log.info("Math.PI : {}", Math.PI);
+
+        assertThat(pi).isGreaterThan(3.14);
     }
 
 
